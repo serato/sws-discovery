3661fa8f5f42c7a708ae318ac7aa6be5779a4da2
Chris Deighton <chris@serato.com>
2020-09-21 16:21:08 +1200

    WEB-2931 First commit of host name discovery class

---
+++ src/HostName.php
@@ -0,0 +1,258 @@
+<?php
+declare(strict_types=1);
+namespace Serato\ServiceDiscovery;
+
+use Exception;
+
+class HostName
+{
+    public const SERATO_COM = 'serato_com';
+    public const MANAGE_SERATO_COM = 'manage_serato_com';
+    public const AUTH_SERATO_COM = 'auth_serato_com';
+    public const STORE_SERATO_COM = 'store_serato_com';
+    public const API_SERATO_COM = 'api_serato.com';
+    public const PLAYLISTS = 'playlists';
+    public const OLD_SCRATCHLIVE_NET = 'OLD_SCRATCHLIVE_NET';
+    public const SERA_TO = 'sera_to';
+    public const IN_SERA_TO_API = 'in_sera_to_api';
+
+    public const MY_ACCOUNT = 'my_account';
+    public const MY_SERATO = 'my_serato';
+    public const SDJ_SIMULATOR = 'sdj_simulator';
+    public const CONSOLE = 'console';
+
+    public const IDENTITY = 'identity';
+    public const LICENSE = 'license';
+    public const ECOM = 'ecom';
+    public const NOTIFICATIONS = 'notifications';
+    public const DIGITAL_ASSETS = 'da';
+    public const PROFILE = 'profile';
+
+    private const ENVIRONMENT_NAMES = ['production', 'test', 'dev'];
+
+    # Defines a list of hosts that make up the SWS web services
+    # The value of each array item is the identifier for each service as passed to
+    # PHP SDK. It's VERY important that these values match.
+    # See https://github.com/serato/sws-php-sdk/blob/master/src/Sdk.php#L21
+    private const SWS_HOSTS = [
+        self::IDENTITY => 'id',
+        self::LICENSE => 'license',
+        self::ECOM => 'ecom',
+        self::NOTIFICATIONS => 'notifications',
+        self::DIGITAL_ASSETS => 'da',
+        self::PROFILE => 'profile'
+    ];
+
+    private const TEST_STACK_NUM_PLACEHOLDER = '__TEST_STACK_NUM__';
+
+    private const HOSTS = [
+        self::SERATO_COM => [
+            'production' => 'https://serato.com',
+            'staging' => 'https://serato.xyz',
+            'preprod' => 'https://serato.biz',
+            'dev' => 'http://192.168.4.2:8080',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '.serato.net'
+        ],
+        self::MANAGE_SERATO_COM => [
+            'production' => 'https://manage.serato.com',
+            'staging' => 'https://manage.serato.xyz',
+            'preprod' => 'https://manage.serato.biz',
+            'dev' => 'http://192.168.4.2:8083',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-manage.serato.net'
+        ],
+        self::AUTH_SERATO_COM => [
+            'production' => 'https://auth.serato.com',
+            'staging' => 'https://auth.serato.xyz',
+            'preprod' => 'https://auth.serato.biz',
+            'dev' => 'http://192.168.4.2:8081',
+            'test' => 'http://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-my.serato.net'
+        ],
+        self::STORE_SERATO_COM => [
+            'production' => 'https://store.serato.com',
+            'staging' => 'https://store.serato.xyz',
+            'preprod' => 'https://store.serato.biz',
+            'dev' => 'http://192.168.2.2',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-store.serato.net'
+        ],
+        self::API_SERATO_COM => [
+            'production' => 'https://api.serato.com',
+            'staging' => 'https://api.serato.xyz',
+            # No DNS entry for this (ie. doesn't exist in pre-prod):
+            'preprod' => 'https://api.serato.biz',
+            'dev' => 'http://192.168.5.2',
+            # No test stacks exist and no DNS entries. So this is made up :-)
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-api.serato.net'
+        ],
+        self::PLAYLISTS => [
+            'production' => 'https://playlists.serato.com',
+            'staging' => 'https://playlists.serato.xyz',
+            'preprod' => 'https://playlists.serato.biz',
+            'dev' => 'http://192.168.4.2:8085',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-playlists.serato.net'
+        ],
+        self::OLD_SCRATCHLIVE_NET => [
+            'production' => 'http://old.scratchlive.net',
+            'staging' => 'https://translation.serato.xyz',
+            'preprod' => 'https://translation.serato.biz',
+            'dev' => 'http://192.168.4.2:8086',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-translation.serato.net'
+        ],
+        self::SERA_TO => [
+            'production' => 'https://sera.to',
+            'staging' => 'https://redirects.serato.xyz',
+            'preprod' => 'http://redirects.serato.biz',
+            'dev' => 'http://192.168.4.2:8084',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-redirects.serato.net'
+        ],
+        self::IN_SERA_TO_API => [
+            'production' => 'https://in.api.sera.to',
+            'staging' => 'https://in-api.serato.xyz',
+            # No DNS entry for this (ie. doesn't exist in pre-prod):
+            'preprod' => 'https://in-api.serato.xyz',
+            'dev' => 'http://192.168.0.2',
+            # No test stacks exist and no DNS entries. So this is made up :-)
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-in-api.serato.net'
+        ],
+
+        # Frontend apps service from the "apps.serato.com" web application
+        # These currently all use the same domain name.
+        self::MY_ACCOUNT => [
+            'production' => 'https://account.serato.com',
+            'staging' => 'https://account.serato.xyz',
+            'preprod' => 'https://account.serato.biz',
+            'dev' => 'http://192.168.4.10',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-apps.serato.net'
+        ],
+        self::MY_SERATO => [
+            'production' => 'https://account.serato.com',
+            'staging' => 'https://account.serato.xyz',
+            'preprod' => 'https://account.serato.biz',
+            'dev' => 'http://192.168.4.10',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-apps.serato.net'
+        ],
+        self::SDJ_SIMULATOR => [
+            'production' => 'https://account.serato.com',
+            'staging' => 'https://account.serato.xyz',
+            'preprod' => 'https://account.serato.biz',
+            'dev' => 'http://192.168.4.10',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-apps.serato.net'
+        ],
+        self::CONSOLE => [
+            'production' => 'https://account.serato.com',
+            'staging' => 'https://account.serato.xyz',
+            'preprod' => 'https://account.serato.biz',
+            'dev' => 'http://192.168.4.10',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-apps.serato.net'
+        ],
+
+        # SWS Web Services
+        self::IDENTITY => [
+            'production' => 'https://id.serato.com',
+            'staging' => 'https://id.serato.xyz',
+            'preprod' => 'https://id.serato.biz',
+            'dev' => 'http://192.168.4.14:8585',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-id.serato.net'
+        ],
+        self::LICENSE => [
+            'production' => 'https://license.serato.com',
+            'staging' => 'https://license.serato.xyz',
+            'preprod' => 'https://license.serato.biz',
+            'dev' => 'http://192.168.4.14:8686',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-license.serato.net'
+        ],
+        self::ECOM => [
+            'production' => 'https://ecom.serato.com',
+            'staging' => 'https://ecom.serato.xyz',
+            'preprod' => 'https://ecom.serato.biz',
+            'dev' => 'http://192.168.4.14:8787',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-ecom.serato.net'
+        ],
+        self::NOTIFICATIONS => [
+            'production' => 'https://notifications.serato.com',
+            'staging' => 'https://notifications.serato.xyz',
+            'preprod' => 'https://notifications.serato.biz',
+            'dev' => 'http://192.168.4.14:8484',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-notifications.serato.net'
+        ],
+        self::DIGITAL_ASSETS => [
+            'production' => 'https://da.serato.com',
+            'staging' => 'https://da.serato.xyz',
+            'preprod' => 'https://da.serato.biz',
+            'dev' => 'http://192.168.4.14:8383',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-da.serato.net'
+        ],
+        self::PROFILE => [
+            'production' => 'https://profile.serato.com',
+            'staging' => 'https://profile.serato.xyz',
+            'preprod' => 'https://profile.serato.biz',
+            'dev' => 'http://192.168.4.14:8282',
+            'test' => 'https://test-' . self::TEST_STACK_NUM_PLACEHOLDER . '-profile.serato.net'
+        ]
+    ];
+
+    /** @var string */
+    private $environmentName;
+
+    /** @var integer */
+    private $environmentNumber;
+
+    public function __construct(string $environmentName, int $environmentNumber)
+    {
+        if (!in_array($environmentName, self::ENVIRONMENT_NAMES)) {
+            throw new Exception(
+                'Invalid environment name `' . $environmentName . '`. Valid environment names are `' .
+                implode('`, `', self::ENVIRONMENT_NAMES) . '`.'
+            );
+        }
+
+        $this->environmentName = $environmentName;
+        $this->environmentNumber = $environmentNumber;
+    }
+
+    /**
+     * Returns a host name, including protocol, for the specified application
+     *
+     * @param string $app
+     * @return string
+     * @throws Exception
+     */
+    public function get(string $app): string
+    {
+        if (!isset(self::HOSTS[$app])) {
+            throw new Exception('Invalid application name `' . $app . '`');
+        } elseif ($this->environmentName === 'production' || $this->environmentName === 'dev') {
+            # 'production' and 'dev'
+            return self::HOSTS[$app][$this->environmentName];
+        } elseif ($this->environmentNumber === 0) {
+            # pre-prod
+            return self::HOSTS[$app]['preprod'];
+        } elseif ($this->environmentNumber === 1) {
+            # staging
+            return self::HOSTS[$app]['staging'];
+        } else {
+            # All other test stacks
+            return str_replace(
+                self::TEST_STACK_NUM_PLACEHOLDER,
+                (string)$this->environmentNumber,
+                self::HOSTS[$app]['test']
+            );
+        }
+    }
+
+    /**
+     * Returns a key/value array of host names for all SWS web services.
+     *
+     * They key is the identifier for each service as used within the PHP SDK
+     * See https://github.com/serato/sws-php-sdk#configuring-the-sdk for more.
+     *
+     * @return array<String>
+     */
+    public function getSwsHosts(): array
+    {
+        $data = [];
+        foreach (self::SWS_HOSTS as $app => $id) {
+            $data[$id] = $this->get($app);
+        }
+        return $data;
+    }
+}
